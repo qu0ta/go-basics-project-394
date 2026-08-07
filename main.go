c65e4adbff93765a8851234600524e8e8c004c0c
Stanislav Gupaliuk <64831049+qu0ta@users.noreply.github.com>
2026-08-07 02:07:08 +0000

    v5

--- main.go
+++ main.go
@@ -5,5 +5,9 @@ import (
 )
 
 func main() {
-	fmt.Println(GeneratePassword(-3, 42, true, true, false))
+	fmt.Println(CheckPassword("abc"))         // => "Слабый пароль (оценка 1 из 5)"
+	fmt.Println(CheckPassword("abcdefgh"))    // => "Слабый пароль (оценка 2 из 5)"
+	fmt.Println(CheckPassword("abcdef1234") ) // => "Средний пароль (оценка 3 из 5)"
+	fmt.Println(CheckPassword("Abcdef1234"))  // => "Надёжный пароль (оценка 4 из 5)"
+	fmt.Println(CheckPassword("Abcdef123!") ) // => "Очень надёжный пароль (оценка 5 из 5)"
 }
--- password_generator.go
+++ password_generator.go
@@ -1,13 +1,17 @@
 package main
+
+import "fmt"
 const (
 	lowercase = "abcdefghijklmnopqrstuvwxyz"
 	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	digits    = "0123456789"
 	special = "!@#$%^&*"
 )
+
 func NextRandom(number int) int {
     return (16807 * number) % 2147483647
 }
+
 func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool) string {
 	if length <= 0 {
 		return ""
@@ -32,4 +36,73 @@ func GeneratePassword(length, seed int, useUppercase, useDigits, useSpecial bool
 		curr = NextRandom(curr)
 	}
 	return result
+}
+
+func CheckPassword(password string) string {
+	var score int
+	if len(password) >= 8 {
+		score++
+	}
+	for _, l := range lowercase {
+		isLowercased := false
+		for _, p := range password {
+			if l == p {
+				score++
+				isLowercased = true
+				break
+			}
+		}
+		if isLowercased {
+			break
+		}
+	}
+	for _, l := range uppercase {
+		isUppercased := false
+		for _, p := range password {
+			if l == p {
+				score++
+				isUppercased = true
+				break
+			}
+		}
+		if isUppercased {
+			break
+		}
+	}
+	for _, l := range digits {
+		isdigitted := false
+		for _, p := range password {
+			if l == p {
+				score++
+				isdigitted = true
+				break
+			}
+		}
+		if isdigitted {
+			break
+		}
+	}
+	for _, l := range special {
+		isSpecialized := false
+		for _, p := range password {
+			if l == p {
+				score++
+				isSpecialized = true
+				break
+			}
+		}
+		if isSpecialized {
+			break
+		}
+	}
+	m := make(map[int]string)
+	m[0] = "Слабый"
+	m[1] = "Слабый"
+	m[2] = "Слабый"
+	m[3] = "Средний"
+	m[4] = "Надёжный"
+	m[5] = "Очень надёжный"
+
+	return fmt.Sprintf("%s пароль (оценка %d из 5)", m[score], score)
+
 }
